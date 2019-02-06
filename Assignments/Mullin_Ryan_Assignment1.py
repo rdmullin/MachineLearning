dbf0f0a85563faab4d60dc830bb40173c112408f
Ryan Mullin <rmullin6@msudenver.edu>
2019-02-06 14:41:53 -0700

    Fix Sample Data

--- Assignments/Mullin_Ryan_Assignment1.py
+++ Assignments/Mullin_Ryan_Assignment1.py
@@ -2,11 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-#Generate Data Samples
-X = np.zeros(10)
-for i in range(10):
-    X[i] = random.randint(1,100)
-    X[i] = (X[i] * 2) + 50
+x = np.random.randint(0,100,10)
+y = (x * 2) + 50
 
-    
+
+
 
